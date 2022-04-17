8f45646630661a4975dd26bfcb39f864a59771ce
OGabrielPereira <ogabrielpereira@pm.me>
2022-04-17 01:38:12 -0300

    ajusts

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-use std::io::BufReader;
-    use std::io::BufRead;
-    use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::fs::File;
                     
-    fn main() {   
-        let f = File::open("poem.txt").unwrap();
-        let file = BufReader::new(&f);
-        println!("\n");
-        for (num, line) in file.lines().enumerate() {
-            let l = line.unwrap();
-            println!("{} - {}", num, l);
-        }           
-        println!("\n");
-    }
+fn main() {   
+    let f = File::open("poem.txt").unwrap();
+    let file = BufReader::new(&f);
+    println!("\n");
+    for (num, line) in file.lines().enumerate() {
+        let l = line.unwrap();
+        println!("{} - {}", num, l);
+    }           
+    println!("\n");
+}
